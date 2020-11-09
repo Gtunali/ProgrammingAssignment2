43d62c44c140bee6ca102c165211a0cc7a55bd59
gcetinte <gcetinte@witsc02t72tcgtf.lan>
2020-11-08 21:05:16 -0500

    removed some comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,6 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
 ## creates a matrix that can cash it's inverse
 makeCacheMatrix <- function(x = matrix()) {
     m_inv <- NULL
@@ -18,7 +17,6 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
 ## Returns the inverse of a matrix by either returning the value from the cache
 ## (if available), or computes it
 cacheSolve <- function(x, ...) {
